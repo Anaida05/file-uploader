91596c3daf77eaff9aee2c26a54e5ebb75bc9349
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-06-05 18:07:41 +0530

    update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,19 +1,55 @@
 import React, { useState } from 'react'
+import axios from "axios"
 import "./App.css"
 const App = () => {
-  const [file, setFile] = useState()
+  const CLOUD_NAME = "dmmy4z3fc"
+  const UPLOAD_PRESET = "unsigned_preset"
+  const [file, setFile] = useState([])
+  const [imageURL,setImageURL] = useState("")
   const handleUpload = (e) => {
     setFile(e?.target?.files[0])
     console.log('e.target', e.target.files)
   }
 
+  const uploadToCloudinary=async(e)=>{
+    e.preventDefault();
+    if(!file){
+      alert("Please select a file!!!")
+      return
+    }
+
+    const formData = new FormData();
+    formData.append("file",file);
+    formData.append("upload_preset", UPLOAD_PRESET);
+
+
+    try{
+      const response = await axios.post(`https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,formData,{
+        headers : {
+          "Content-type" : "multipart/form-data"
+        }}
+      )
+       setImageURL(response.data.secure_url);
+      console.log('response.data', response.data)
+    }catch(error){
+      console.log("Failed",error)
+    }
+  }
+
   return (
-    <div>
-      <form >
-        <h2>File Uploader</h2>
-        <input type="file" onChange={handleUpload} />
-        <button type='submit'>Upload</button>
-      </form>
+    <div style={{ padding: "2rem" }}>
+      <h2>Upload Files</h2>
+      <input type="file" multiple onChange={handleUpload}/>
+      <button onClick={uploadToCloudinary}>Upload</button>
+
+      {
+        imageURL && (
+          <div>
+            <h3>Image Uploaded</h3>
+            <img src={imageURL} alt="Uploaded" style={{ width: "200px" }}/>
+          </div>
+        )
+      }
     </div>
   )
 }
